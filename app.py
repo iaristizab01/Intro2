951e3237da51dd5d90a7d96b568e3b0daca04092
iaristizab01 <isabelaaristizabal01@gmail.com>
2025-08-28 10:04:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from PIL import image 
+from PIL import Image 
 
 st.title("Mi primera App!")
 
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente piuedo desarrollar backend y frontend.")
-image=Imgae.open("Hello-Kitty.jpg")
+image=Image.open("Hello-Kitty.jpg")
 
 st.image(image, caption='Interfaces multimodales')
